b50dbb8c49d206e51a7c8ed89be2b50850707a1c
Ummu Rasul <ummurasul5@gmail.com>
2019-07-26 16:45:17 +0300

    added requirements

--- courses/urls.py
+++ courses/urls.py
@@ -5,5 +5,5 @@ app_name = 'courses'
 
 urlpatterns = [
     path('', CourseListView.as_view(), name='list'),
-    path('<slug>', CourseDetailView.as_view(), namespace='detail'),
+    path('<slug>', CourseDetailView.as_view(), name='detail'),
 ]
